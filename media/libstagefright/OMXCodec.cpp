d78a30b9f175674a4833e19763677cf249bc20b2
Leena Winterrowd <lenhardw@codeaurora.org>
2013-11-26 17:05:04 -0800

    libstagefright: Enable HFR feature for video recording
    - Enable HFR related calls to set HFR mode in stagefright and
      libmediaplayerservice
    - Perform HFR scaling with mp4 timescale parameter instead of
      recalculating timestamps
    
    Change-Id: Ie30637093d91ed7f5a41c5137c3ff45e1370a906
    
    libstagefright: HSR: Check if HSR enabled, use HFR value for HSR
    
    - HSR is high speed recording hint from camera app, if this is set
      use frame rate set in HFR as normal encoder frame rate.
    - HSR hint also indicates that this is not HFR feature and treat
      this encoding as normal encoding
    
    Change-Id: Ifff59cd8ed88efca230fd34e46325a11728e8c86
    
    libmedia: Adding HFR entry in MediaProfiles.cpp
    
    -- Adding HFR entries for max HFR mode, max HFR width & max HFR height
       in MediaProfiles.cpp
    
    CRs-Fixed: 589983
    Change-Id: Iafcc1be4124879a029a2e4b8aded35150d0c120b
    
    libstagefright: Use hw encoder for video recording
    
    Use hw encoder for video recording based on the platform.
    
    CRs-Fixed: 576174
    Change-Id: I5d14d2fabb8d39fe940fa25c88b90252bfd07bc0
    
    libstagefright: Fixed typo in target name
    
    Target name was wrong which was causing this flag to be not set thereby
    software codecs were also being used
    
    Change-Id: I3f3a0de4eda0fef2f2f26579645a4a06a1ecddaf
    CRs-Fixed: 582977
    
    stagefright: Correct total duration of HFR clips
    
    HFR leverages the mp4 track timescale parameter which is not
    considered when calculating total clip duration. This makes tracks
    report the correct duration so that total clip duration is computed
    correctly.
    
    Change-Id: I8ff556913317eea15030f9f6920cefa560133872
    
    libstagefright: Cleanup HFR cruft
    
    Change-Id: I67d43cd2e14f33e2915afd61b590c32d7c97a9c6

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -1051,10 +1051,6 @@ status_t OMXCodec::setVideoInputFormat(
     CHECK(success);
     CHECK(stride != 0);
 
-#ifdef QCOM_HARDWARE
-    ExtendedUtils::HFR::reCalculateHFRParams(meta, frameRate, bitRate);
-#endif
-
     OMX_VIDEO_CODINGTYPE compressionFormat = OMX_VIDEO_CodingUnused;
     if (!strcasecmp(MEDIA_MIMETYPE_VIDEO_AVC, mime)) {
         compressionFormat = OMX_VIDEO_CodingAVC;
@@ -1319,10 +1315,6 @@ status_t OMXCodec::setupH263EncoderParameters(const sp<MetaData>& meta) {
     h263type.nAllowedPictureTypes =
         OMX_VIDEO_PictureTypeI | OMX_VIDEO_PictureTypeP;
 
-#ifdef QCOM_HARDWARE
-    ExtendedUtils::HFR::reCalculateHFRParams(meta, frameRate, bitRate);
-#endif
-
     h263type.nPFrames = setPFramesSpacing(iFramesInterval, frameRate);
     if (h263type.nPFrames == 0) {
         h263type.nAllowedPictureTypes = OMX_VIDEO_PictureTypeI;
@@ -1373,10 +1365,6 @@ status_t OMXCodec::setupMPEG4EncoderParameters(const sp<MetaData>& meta) {
 
     mpeg4type.nAllowedPictureTypes =
         OMX_VIDEO_PictureTypeI | OMX_VIDEO_PictureTypeP;
-
-#ifdef QCOM_HARDWARE
-    ExtendedUtils::HFR::reCalculateHFRParams(meta, frameRate, bitRate);
-#endif
 
     mpeg4type.nPFrames = setPFramesSpacing(iFramesInterval, frameRate);
     if (mpeg4type.nPFrames == 0) {
@@ -1438,10 +1426,6 @@ status_t OMXCodec::setupAVCEncoderParameters(const sp<MetaData>& meta) {
     if (err != OK) return err;
     h264type.eProfile = static_cast<OMX_VIDEO_AVCPROFILETYPE>(profileLevel.mProfile);
     h264type.eLevel = static_cast<OMX_VIDEO_AVCLEVELTYPE>(profileLevel.mLevel);
-
-#ifdef QCOM_HARDWARE
-    ExtendedUtils::HFR::reCalculateHFRParams(meta, frameRate, bitRate);
-#endif
 
     // XXX
 #ifdef USE_TI_DUCATI_H264_PROFILE
