1399ed2cf07cceff10ee634e5681f99b547fc2e1
Konsta <konsta09@gmail.com>
2013-12-29 12:52:49 +0200

    support legacy qcom audio variant
    *msm7x27a: build lpa player & don't use tunnel audio
    
    Change-Id: I3a1b7b9b6e2646b3ddfb34d7f136589c965b5a29

--- media/libstagefright/Android.mk
+++ media/libstagefright/Android.mk
@@ -78,7 +78,7 @@ else
 LOCAL_C_INCLUDES += $(TOP)/frameworks/native/include/media/openmax
 endif
 
-ifneq ($(filter caf bfam,$(TARGET_QCOM_AUDIO_VARIANT)),)
+ifneq ($(filter caf bfam legacy,$(TARGET_QCOM_AUDIO_VARIANT)),)
     ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
         ifeq ($(call is-chipset-in-board-platform,msm8960),true)
             LOCAL_SRC_FILES += LPAPlayerALSA.cpp TunnelPlayer.cpp
@@ -96,6 +96,10 @@ ifneq ($(filter caf bfam,$(TARGET_QCOM_AUDIO_VARIANT)),)
             LOCAL_CFLAGS += -DLEGACY_LPA -DUSE_LPA_MODE
         endif
         ifeq ($(call is-chipset-in-board-platform,msm7x30),true)
+            LOCAL_SRC_FILES += LPAPlayer.cpp
+            LOCAL_CFLAGS += -DLEGACY_LPA -DUSE_LPA_MODE
+        endif
+        ifeq ($(call is-chipset-in-board-platform,msm7x27a),true)
             LOCAL_SRC_FILES += LPAPlayer.cpp
             LOCAL_CFLAGS += -DLEGACY_LPA -DUSE_LPA_MODE
         endif
