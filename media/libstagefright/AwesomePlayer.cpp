Jackeagle/caf_frameworks_av

libstagefright: adjust AV sync late margin based on property
- Current AV late margin is 40 ms
- Read system property media.sf.set.late.margin and if
  new AV sync late margin is set then use the same
- Max AV sync late margin allowed is 250 ms
- Min AV sync late margin is default 40 ms

Change-Id: I65cd3dce9b31b87f1488c37af9618669b29c362c

media/libstagefright/AwesomePlayer.cpp
@@ -232,6 +232,7 @@ AwesomePlayer::AwesomePlayer()
 
     reset();
     mIsTunnelAudio = false;
+    mLateAVSyncMargin = QCUtils::ShellProp::getMaxAVSyncLateMargin();
 }
 
 AwesomePlayer::~AwesomePlayer() {
@@ -2120,7 +2121,7 @@ void AwesomePlayer::onVideoEvent() {
             }
         }
 
-        if (latenessUs > 40000) {
+        if (latenessUs > mLateAVSyncMargin) {
             // We're more than 40ms late.
             ALOGV("we're late by %lld us (%.2f secs)",
                  latenessUs, latenessUs / 1E6);

xsalahx/android_frameworks_av

media/libstagefright/AwesomePlayer.cpp
@@ -259,6 +259,8 @@ AwesomePlayer::AwesomePlayer()
 #ifdef QCOM_DIRECTTRACK
     mIsTunnelAudio = false;
 #endif
+
+    mLateAVSyncMargin = ExtendedUtils::ShellProp::getMaxAVSyncLateMargin();
 }
 
 AwesomePlayer::~AwesomePlayer() {
@@ -2316,7 +2318,7 @@ void AwesomePlayer::onVideoEvent() {
             }
         }
 
-        if (latenessUs > 40000) {
+        if (latenessUs > mLateAVSyncMargin) {
             // We're more than 40ms late.
             ALOGV("we're late by %lld us (%.2f secs)",
                  latenessUs, latenessUs / 1E6);